63d2820bddb44c48510591b4238bf4b9acfede23
Lobst3rG3off <erip.pasei@gmail.com>
2019-05-30 20:14:47 -0400

    api works

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ class App extends Component {
     this.state = {
       meme: [],
       isLoading: true,
-      outputData: 0,
-      outputDataLength: 0,
+      // outputData: 0,
+      // outputDataLength: 0,
       memetoAppend: [],
       isClicked: false,
 
@@ -25,53 +25,59 @@ class App extends Component {
 
   componentDidMount() {
     // perform ajax request after component has finished rendering
-    axios({
-      url: 'api.giphy.com/v1/gifs/trending',
-      method: 'GET',
-      dataResponse: 'JSON',
-      params: {
-        // maybe apikey if needed
-        q: 'meme',
-        api_key: 'XI2UVtaoFcAmwfGg9S1bcArrtsCLVxPc',
-        fmt: 'JSON',
-      }
-    }).then((response) => {
-      //store the date pulled from the ajax call into the empty array, and only parsing to the stuff We want
-      response = response.data.data.memes;
-      const outputData = response;
-      const outputDataLength = outputData.length;
-      console.log(response);
-      console.log(outputDataLength);
-      this.setState({
-        // change meme to memeOG
-        meme: response,
-        isLoading: false,
-        outputData: outputData,
-        outputDataLength: outputData.length,
+    // axios({
+    //   url: 'http://api.giphy.com/v1/gifs/trending',
+    //   method: 'GET',
+    //   dataResponse: 'JSON',
+    //   params: {
+    //     // maybe apikey if needed
+    //     q: 'meme',
+    //     api_key: 'XI2UVtaoFcAmwfGg9S1bcArrtsCLVxPc',
+    //     fmt: 'JSON',
+    //   }
+    // }).then((response) => {
+    //   //store the date pulled from the ajax call into the empty array, and only parsing to the stuff We want
+    //   response = response.data.data.memes;
+    //   // const outputData = response;
+    //   // const outputDataLength = outputData.length;
+    //   console.log(response);
+    //   // console.log(outputDataLength);
+    //   this.setState({
+    //     // change meme to memeOG
+    //     meme: response,
+    //     isLoading: false,
+    //     // outputData: outputData,
+    //     // outputDataLength: outputData.length,
 
-      }  
-      )
-      // this.randomimzer(response);
-    })
+    //   }  
+    //   )
+    //   // this.randomimzer(response);
+    // })
 
-  }
 
-  randomimzer = (response) => {
-    console.log('Randomizer activate!' , this.state.meme) 
-    const allMemes = [...this.state.meme];
+    axios.get("http://api.giphy.com/v1/gifs/trending?api_key=XI2UVtaoFcAmwfGg9S1bcArrtsCLVxPc&offset=0")
+      .then((response) => {
+          console.log(response);
+          }
+
+      )}
+
+  // randomimzer = (response) => {
+  //   console.log('Randomizer activate!' , this.state.meme) 
+  //   const allMemes = [...this.state.meme];
     
-    for (let i = allMemes.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [allMemes[i], allMemes[j]] = [allMemes[j], allMemes[i]];
-    }
-    console.log(allMemes);
+  //   for (let i = allMemes.length - 1; i > 0; i--) {
+  //     const j = Math.floor(Math.random() * (i + 1));
+  //     [allMemes[i], allMemes[j]] = [allMemes[j], allMemes[i]];
+  //   }
+  //   console.log(allMemes);
 
-    const memeSlice = allMemes.slice(0,6)
-    console.log(memeSlice);
+  //   const memeSlice = allMemes.slice(0,6)
+  //   console.log(memeSlice);
 
-    this.setState({
-      memetoAppend: memeSlice
-    })
+  //   this.setState({
+  //     memetoAppend: memeSlice
+  //   })
 
     
 
@@ -115,7 +121,7 @@ class App extends Component {
     //   } 
     // }
 
-  }
+  // }
 
   // memeGenerator = () => {
 
@@ -136,34 +142,35 @@ class App extends Component {
   //   )}
   // }
 
-  handleClick = () => {
+//   handleClick = () => {
 
-let meme = this.state.meme
+// let meme = this.state.meme
 
-    this.randomimzer(meme);
-  }
+//     this.randomimzer(meme);
+//   }
   
   render() {
      
         
     return (
       <div className="App" >
-        <h1> Dank Memes! </h1>
+        <h1> Dank Memes! </h1> 
 
         
-              <button className="button" onClick={this.handleClick} >
+              {/* <button className="button" onClick={this.handleClick} >
                 Let's Make Some Memes!
               </button>
 
               { this.state.memetoAppend.map((template) => {
         return <MemeBridge title={template.name} imgUrl={template.url} />
       })
-    }
+    } */} 
               
       </div>
     )
   }
 }
+
 
 
 

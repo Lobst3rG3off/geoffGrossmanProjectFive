4b9d4e67cd81f479650c76dc41463caa046661a6
Lobst3rG3off <erip.pasei@gmail.com>
2019-06-02 16:44:43 -0400

    project sorta final

--- src/App.js
+++ src/App.js
@@ -24,36 +24,6 @@ class App extends Component {
   }
 
   componentDidMount() {
-    // perform ajax request after component has finished rendering
-    // axios({
-    //   url: 'http://api.giphy.com/v1/gifs/trending',
-    //   method: 'GET',
-    //   dataResponse: 'JSON',
-    //   params: {
-    //     // maybe apikey if needed
-    //     q: 'meme',
-    //     api_key: 'XI2UVtaoFcAmwfGg9S1bcArrtsCLVxPc',
-    //     fmt: 'JSON',
-    //   }
-    // }).then((response) => {
-    //   //store the date pulled from the ajax call into the empty array, and only parsing to the stuff We want
-    //   response = response.data.data.memes;
-    //   // const outputData = response;
-    //   // const outputDataLength = outputData.length;
-    //   console.log(response);
-    //   // console.log(outputDataLength);
-    //   this.setState({
-    //     // change meme to memeOG
-    //     meme: response,
-    //     isLoading: false,
-    //     // outputData: outputData,
-    //     // outputDataLength: outputData.length,
-
-    //   }  
-    //   )
-    //   // this.randomimzer(response);
-    // })
-
 
     axios.get("http://api.giphy.com/v1/gifs/trending?api_key=XI2UVtaoFcAmwfGg9S1bcArrtsCLVxPc&offset=0")
      .then((response) => {
@@ -92,44 +62,37 @@ class App extends Component {
     this.setState({
       memetoAppend: memeSlice
     })
+    
+
   }
 
-  // memeGenerator = () => {
-
-  //   console.log('Randomizer activate!' , this.state.memes) 
-  //   const allMemes = [...this.state.memes];
-    
-  //   for (let i = allMemes.length - 1; i > 0; i--) {
-  //     const j = Math.floor(Math.random() * (i + 1));
-  //     [allMemes[i], allMemes[j]] = [allMemes[j], allMemes[i]];
-  //   }
-  //   console.log(allMemes);
-
-  //   const memeSlice = allMemes.slice(0,6)
-  //   console.log(memeSlice);
-
-  //   this.setState({
-  //     memetoAppend: memeSlice
-  //   }
-  //   )}
-  
 
   handleClick = () => {
 console.log('clicked!')
 let meme = this.state.memes
 
     this.randomimzer(meme);
+
+    
+  document.getElementById("gallery").scrollIntoView();
   }
+
+  handledClick = () => {
+    console.log('clicked!')
+    document.getElementById("heroImage").scrollIntoView();
+  }
+
+
   
   render() {
      
         
     return (
       <div className="App" >
-        <div className="heroImage wrapper">
+        <div className="heroImage" id="heroImage">
           <h1> Trending Gifs </h1> 
           <h2>Any way you say it, they are awesome!</h2>
-          <h3>Click the Button to see the most trending GIF's</h3>
+          <h2>See the most trending GIFs</h2>
         
         
                <button className="button" onClick={this.handleClick} >
@@ -137,11 +100,15 @@ let meme = this.state.memes
               </button>
       </div>
 
-            <div className="gallery wrapper">
-              { this.state.memetoAppend.map((template) => {
-                  return <MemeBridge title={template.title} imgUrl={template.images.original.url} />
-                })
-              }  
+            <div className="gallery" id="gallery" >
+              <div className="wrapper">
+                { this.state.memetoAppend.map((template, i) => {
+                    return <MemeBridge key={i} title={template.title} imgUrl={template.images.original.url} />
+                  })
+                }  
+                 <button className="button" onClick={this.handledClick}>Back to Top</button>
+                </div>
+               
             </div>   
       </div>
     )
